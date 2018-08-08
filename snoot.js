2919137062b652c2eb5bc0c11350faee99d7e276
Unknown <MSandoval910@west-mec.org>
2018-08-07 18:24:33 -0700

    commit
    more functions too update the days and the months and the years and added 3 global variables.

--- snoot.js
+++ snoot.js
@@ -9,18 +9,65 @@
 
 "use strict"
 
+var twentyNine = document.createDocumentFragment(); 
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // function to turn off select list defaults
 
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    alert("Select lists: " + emptyBoxes.length);
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;   
+    }
 }
- 
+// function too set up document fragments for days of month
+function setUpDays() {
+// get the days option tags
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+// function to update the days select list
+
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    if (deliveryMonth.selectedIndex === -1) {
+        deliveryMonth.selectedIndex = 0;
+    }
+    // if Feb and 202o - leap year 
+    
+    // else 30 day month  - thirty
+    
+    // else 31 month - thirtyOne
+}
+
+// functions to run on page load
+
+function setUpPage() {
+    removeSelectDefaults();
+//    setUpDays();
+//    updateDays();
+    
+}
 
 // enable load event handlers 
 
-if (windew.addEventListener) {
-    window.addEventListener("load" , removeSelectDefaults, false)
+if (window.addEventListener) {
+    window.addEventListener("load" , setUpPage, false)
 }  else if (window.attachEvent) {
-   window.attachEvent("onload" , removeSelectDefaults) 
+   window.attachEvent("onload" , setUpPage) 
 }
+
